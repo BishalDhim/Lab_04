e96a0c19059ce78610e8fe68b046912c67362995
621781 <bishaldhimal09@gmail.com>
2025-09-08 08:55:41 -0400

    test

--- src/Task1.java
+++ src/Task1.java
@@ -1,4 +1,16 @@
 public class Task1 {
     public static void main(String[] args) {
+        //Variable Declaration
+        double doubleOperandA = 5;
+        double doubleOperandB = 8;
+        double doubleSum = doubleOperandA + doubleOperandB;
+        double doubleProduct;
+        double doubleDifference;
+        double doubleQuotient;
+        double doubleModulo;
 
+        doubleSum = doubleOperandA + doubleOperandB;
+
+        System.out.println("The sum using double units is"+ doubleOperandA + "" + doubleOperandB + "is" + doubleSum);
     }
+}
