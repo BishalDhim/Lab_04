7fe303380718ad77cd401f54e921e97df3cd2bad
621781 <bishaldhimal09@gmail.com>
2025-09-09 08:54:17 -0400

    test

--- src/Task2.java
+++ src/Task2.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+    //Variable Declarations
+       double salesTax = .05;
+       double purchasePrice;
+       double n;
+       Scanner scan = new Scanner(System.in);
+
+
+
+        System.out.println(" please input the price of your purchase value: ");
+        n = scan.nextDouble();
+        System.out.println();
+
+
+
 
     }
 }
