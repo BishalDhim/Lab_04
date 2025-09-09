9b26dff7d34ee52fdf4fbdd63b978582c99dc4e8
621781 <bishaldhimal09@gmail.com>
2025-09-09 08:33:35 -0400

    test

--- src/Task1.java
+++ src/Task1.java
@@ -1,15 +1,25 @@
+import java.sql.SQLOutput;
+
 public class Task1 {
     public static void main(String[] args) {
         //Variable Declaration
         double doubleOperandA = 5;
         double doubleOperandB = 8;
         double doubleSum = doubleOperandA + doubleOperandB;
-        double doubleProduct;
-        double doubleDifference;
-        double doubleQuotient;
-        double doubleModulo;
+        double doubleProduct = doubleOperandA * doubleOperandB;
+        double doubleDifference = doubleOperandA - doubleOperandB;
+        double doubleQuotient = doubleOperandA / doubleOperandB ;
+        double doubleModulo = doubleOperandA % doubleOperandB ;
 
 
-        System.out.println("The sum using double units is " + doubleOperandA + " and " + doubleOperandB + " equals " + doubleSum);
+        System.out.println("The sum using double units is " + doubleSum);
+
+        System.out.println("The product using double units is " + doubleProduct);
+
+        System.out.println("The difference using double units is " + doubleDifference);
+
+        System.out.println("The quotient using double units is " + doubleQuotient);
+
+        System.out.println("The modulo using double units is " + doubleModulo);
     }
 }
