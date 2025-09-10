31c231f8ab63085858c7d700361153b4e5fbb637
621781 <bishaldhimal09@gmail.com>
2025-09-10 08:26:58 -0400

    test

--- src/Task2.java
+++ src/Task2.java
@@ -4,15 +4,25 @@ public class Task2 {
     public static void main(String[] args) {
     //Variable Declarations
        double salesTax = .05;
-       double purchasePrice;
-       double n;
+       double totalPrice;
+       double taxAmount;
+
        Scanner scan = new Scanner(System.in);
 
 
 
         System.out.println(" please input the price of your purchase value: ");
-        n = scan.nextDouble();
+        double purchasePrice = scan.nextDouble();
         System.out.println();
+
+        taxAmount = purchasePrice * salesTax;
+        totalPrice = purchasePrice + taxAmount;
+
+        System.out.println(" Your price after the 5% sales tax is " + totalPrice);
+
+
+
+
 
 
 
